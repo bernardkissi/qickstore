b8fcad9dd3dc8905c5537faba00f828fe4bd3d23
bernardk23 <bernardkissi@live.com>
2021-06-02 14:10:29 +0000

    Added shipped state to orders

---
+++ app/Domains/Orders/States/Shipped.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domains\Orders\States;
+
+class Shipped extends OrderState
+{
+    /**
+    * Property name of this state in DB
+    *
+    * @var string
+    */
+    public static $name = 'shipped';
+
+    /**
+    * Returns the paid state of an order
+    *
+    * @return string
+    */
+    public function status(): string
+    {
+        return 'Shipped';
+    }
+}
