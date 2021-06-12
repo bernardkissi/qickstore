79a9f73e301adc931d1441ed6d2e276a30c5767d
bernardk23 <bernardkissi@live.com>
2021-06-12 16:09:59 +0000

    Add configurable modules to config modules

--- config/modules.php
+++ config/modules.php
@@ -1,10 +1,21 @@
 <?php
 
+/*
+|--------------------------------------------------------------------------
+| APP Modules
+|--------------------------------------------------------------------------
+|
+| Here are the application modules which are registered with the application
+| on loading. Feel free to register or replace any module which will be resolved
+| and register in the service container.
+*/
+
 return [
 
-    'cart'=> [
-
-        'vcart' => '\App\Domains\Cart\Services\Cart'
+    'cart'=> ['vcart' => App\Domains\Cart\Services\Cart::class],
+    'checkouts' => [
+        'download' => App\Domains\Orders\Checkouts\Downloading\DownloadService::class,
+        'shipping' => App\Domains\Orders\Checkouts\Shipping\ShippingService::class
     ]
 
 ];
