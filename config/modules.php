ae77ee3423ff48a4d8d1abaa19d00482b70500ba
bernardk23 <bernardkissi@live.com>
2021-06-20 11:26:25 +0000

    updated modules configuration

--- config/modules.php
+++ config/modules.php
@@ -12,10 +12,61 @@
 
 return [
 
-    'cart'=> ['vcart' => App\Domains\Cart\Services\Cart::class],
-    'checkouts' => [
-        'download' => App\Domains\Orders\Checkouts\Downloading\DownloadService::class,
-        'shipping' => App\Domains\Orders\Checkouts\Shipping\ShippingService::class
+    /*
+    |--------------------------------------------------------------------------
+    | Store Cart System
+    |--------------------------------------------------------------------------
+    |
+    | This service acts as the cart system for stores. feel free to swap with
+    | different cart service.
+    */
+
+    'cart'=> [
+        'vcart' => App\Domains\Cart\Services\Cart::class
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Checkout Gateways
+    |--------------------------------------------------------------------------
+    |
+    | Checkout gateway acts a the checkout service used to checkout the customer.
+    | Feel free to add checkout services to this service to handle different
+    | orders or swap default.
+    */
+
+    'checkout' => [
+        'default' => App\Domains\Orders\Checkouts\Services\CheckoutService::class
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Delivery Channels
+    |--------------------------------------------------------------------------
+    |
+    | Below are the current the delivery channels available to stores. Feel
+    | free to add more channels to stores.
+    */
+
+    'deliveries' => [
+        'swoove' => App\Domains\Delivery\Services\SwooveDelivery::class,
+        'hosted' => App\Domains\Delivery\Services\HostedDelivery::class,
+        'files'  => App\Domains\Delivery\Services\FilesDelivery::class
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Tracking Services
+    |--------------------------------------------------------------------------
+    |
+    | Tracking services for the delivery channels defined above. Add a tracking
+    | service when you add a new delivery channel.
+    */
+
+    'tracking' => [
+        'files' => App\Domains\Tracking\DeliveryServices\FilesTracking::class,
+        'swoove' => App\Domains\Tracking\DeliveryServices\SwooveTracking::class,
+        'hosted' => App\Domains\Tracking\DeliveryServices\HostedTracking::class,
     ]
 
 ];
