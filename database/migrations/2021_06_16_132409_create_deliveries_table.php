84955a5d7a9cadcb9400b0385a2416698d14d9b8
bernardk23 <bernardkissi@live.com>
2021-06-20 11:26:53 +0000

    Added payments and deliveries migrations

---
+++ database/migrations/2021_06_16_132409_create_deliveries_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDeliveriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('deliveries', function (Blueprint $table) {
+            $table->id();
+            $table->string('service');
+            $table->foreignId('order_id')->unsigned()->index()->constrained('orders');
+            $table->integer('amount')->default(0);
+            $table->string('state');
+            $table->string('delivery_code');
+            $table->string('client_code')->nullable();
+            $table->string('estimate_id')->nullable();
+            $table->text('instructions')->nullable();
+            $table->string('download_link')->nullable();
+            $table->text('error')->nullable();
+            $table->json('delivery_address')->nullable();
+            $table->json('delivery_details')->nullable();
+            $table->dateTime('completed_at')->nullable();
+            $table->dateTime('failed_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('deliveries');
+    }
+}
