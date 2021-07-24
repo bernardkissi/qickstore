c8eee5b88bfb7042117466c5a17386293d99afaa
bernardk23 <bernardkissi@live.com>
2021-07-24 02:22:07 +0000

    Order notifications modified

---
+++ app/Domains/Orders/Notifications/CustomerOrderNotification.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Domains\Orders\Notifications;
+
+use App\Domains\Services\Notifications\Channels\SmsChannel;
+use App\Domains\Services\Notifications\Types\Sms\SmsMessage;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Notification;
+
+class CustomerOrderNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(public string $message)
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return [SmsChannel::class];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return SmsMessage
+     */
+    public function toSms($notifiable)
+    {
+        return (new SmsMessage)
+                    ->from('Techshops')
+                    ->to('0543063709') //TODO: using notifiable grab the customer number
+                    ->line($this->message)
+                    ->line('Thank you for shopping with us!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ app/Domains/Orders/Notifications/DemoCallNotification.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Domains\Orders\Notifications;
+
+use App\Domains\Services\Notifications\Channels\VoiceChannel;
+use App\Domains\Services\Notifications\Types\Voice\VoiceMessage;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class DemoCallNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return [VoiceChannel::class];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toCall($notifiable)
+    {
+        return (new VoiceMessage)
+                    ->to('0543063709')
+                    ->audio('audio here');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
