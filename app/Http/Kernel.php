4a1e7e8a51ea6872553de1c02df406cfe8e1d449
bernardk23 <bernardkissi@live.com>
2021-09-07 21:01:28 +0000

    Added cart sync middleware to check avaliabilty

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'customer' => \App\Http\Middleware\SwitchCustomer::class,
+        'cart.sync' => \App\Http\Middleware\CartSync::class,
     ];
 }
---
+++ app/Http/Middleware/CartSync.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Domain\Cart\Facade\Cart;
+use Illuminate\Http\Request;
+
+class CartSync
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        Cart::sync();
+
+        if (Cart::hasChanged()) {
+            return response()->json(
+                ['message' => 'Item quantity in cart has reduced, due to a place order'],
+                409
+            );
+        }
+        return $next($request);
+    }
+}
