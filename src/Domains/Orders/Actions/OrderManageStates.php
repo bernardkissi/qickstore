d317981032ec6a5ad093cf16593dcd4860a088ad
bernardk23 <bernardkissi@live.com>
2021-09-27 07:33:21 +0000

    Added Manage state action class

---
+++ src/Domains/Orders/Actions/OrderManageStates.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Domain\Orders\Actions;
+
+use Domain\Orders\Order;
+use Domain\Orders\OrderStatus;
+use Illuminate\Http\JsonResponse;
+
+class OrderManageStates
+{
+    /**
+     * Changes order state
+     *
+     * @param Order $order
+     * @param string $state
+     * @return void
+     */
+    public static function changeState(Order $order, string $state): array|JsonResponse
+    {
+        $status = $order->status;
+        $trans = $order->transition($status, $state);
+
+        if ($trans) {
+            return static::formatTimeline($status->updated_from);
+        }
+        return response()->json(
+            ['message' => "The order is already in $state or is not a supported transition"],
+            401
+        );
+    }
+
+    /**
+     * Returns all state an order has undergone through
+     *
+     * @param Order $order
+     * @return array
+     */
+    public static function getTimeline(Order $order): array
+    {
+        $statuses = $order->status->updated_from;
+        return static::formatTimeline($statuses);
+    }
+
+    /**
+     * Sort the statuses with time updated
+     *
+     * @param array $updates
+     * @return array
+     */
+    private static function formatTimeline(array $updates): array
+    {
+        return collect($updates)
+            ->sortByDesc('time')
+            ->reverse()
+            ->all();
+    }
+}
