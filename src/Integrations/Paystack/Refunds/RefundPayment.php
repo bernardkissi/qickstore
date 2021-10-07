7e70720e7b8f56e55432a1f4118ea891cddf9856
bernardk23 <bernardkissi@live.com>
2021-10-07 09:59:31 +0000

    Add refund request to paystack

---
+++ src/Integrations/Paystack/Refunds/RefundPayment.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Integration\Paystack\Refunds;
+
+use Integration\Paystack\PaystackRequest;
+
+class RefundPayment extends PaystackRequest
+{
+    /**
+    * Base url for the endpoint for the request
+    *
+    * @var string $baseUrl
+    */
+    protected string $method = 'POST';
+
+    /**
+     * Request path to the endpoint
+     *
+     * @var string $query
+     */
+    protected string $path = '/refund';
+}
