5ff0c28dcef5e38d342b2eb617d47295b035babe
bernardk23 <bernardkissi@live.com>
2021-10-10 10:34:34 +0000

    fix notification import

--- src/Domains/Disputes/Jobs/DisputeReminderJob.php
+++ src/Domains/Disputes/Jobs/DisputeReminderJob.php
@@ -9,7 +9,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Notification;
+use Illuminate\Support\Facades\Notification;
 use Service\Notifications\Channels\SmsChannel;
 
 class DisputeReminderJob implements ShouldQueue
